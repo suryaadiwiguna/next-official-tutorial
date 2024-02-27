e62e3ed76bf94e8a1f33b526f0ab9f200430f2f7
Surya Adi Wiguna <142723064+suryaadiwiguna@users.noreply.github.com>
2024-02-27 11:21:50 +0700

    feat: pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -18,6 +18,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     } else {
       params.delete('query')
     }
+    console.log(params.toString())
     replace(`${pathname}?${params.toString()}`)
   }, 500)
 
